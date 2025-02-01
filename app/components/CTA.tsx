e6e024f7e690b611de97c3c2ad8a0aeed663f86b
eimaam <imamddahir@gmail.com>
2025-02-01 07:15:11 +0100

    fix(cta): adjust styles for responsiveness and improve text sizing in CTA component

--- app/components/CTA.tsx
+++ app/components/CTA.tsx
@@ -2,12 +2,12 @@
 
 const CTA = () => {
   return (
-    <section className="mt-8 bg-secondary hover:bg-primary rounded-lg">
+    <section className="mt-8 bg-secondary hover:bg-primary rounded-md md:2rounded-lg">
       <div className="flex flex-col items-center justify-center p-4 space-y-8 md:p-10 lg:space-y-0 lg:flex-row lg:justify-between">
-        <h3 className="text-3xl font-semibold leadi text-center lg:text-left text-white">
+        <h3 className="text-xl md:text-3xl font-semibold leading text-center lg:text-left text-white">
           The TokenStork Flipstarter is now available!
         </h3>
-        <button className="!text-primary px-8 py-3 text-lg font-semibold rounded bg-white">
+        <button className="!text-primary px-8 py-3 text-base md:text-lg font-semibold rounded bg-white">
           <a
             className="no-underline !text-primary"
             href="https://flipstarter.tokenstork.com/en"
